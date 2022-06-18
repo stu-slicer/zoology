b599f82a3a66cad60f7fd710a03e6f4849ab7786
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-06-18 11:54:16 +0100

    Fix issue with no stars

--- src/main/java/com/nology/zoology/star/StarUtils.java
+++ src/main/java/com/nology/zoology/star/StarUtils.java
@@ -10,11 +10,11 @@ public class StarUtils {
     public static final String OUTLINED_STAR = "\u2690"; //⚝
 
     public static String printStars(Starrable starrable) {
-        return printStars( starrable.getStars(), -1 );
+        return printStars(starrable.getStars(), -1);
     }
 
     public static String printStars(Starrable starrable, int outOf) {
-        return printStars( starrable.getStars(), outOf );
+        return printStars(starrable.getStars(), outOf);
     }
 
     public static String printStars(int stars) {
@@ -23,22 +23,36 @@ public class StarUtils {
 
     public static String printStars(int stars, int outOf) {
 
+        StringBuilder sb = new StringBuilder(" ");
+
         if (stars == 0) {
+            if (outOf > 0) {
+                for (int i = 0; i < outOf; i++) {
+                    sb.append(ColourUtils.yellow(WHITE_STAR) + " ");
+                }
+                return sb.toString();
+            }
             return ColourUtils.yellow(WHITE_STAR);
         }
 
-        StringBuilder sb = new StringBuilder(" ");
 
         for (int i = 0; i < stars; i++) {
-            sb.append( ColourUtils.yellow(BLACK_STAR) + " " );
+            sb.append(ColourUtils.yellow(BLACK_STAR) + " ");
         }
-        if( outOf > 0 ) {
+        if (outOf > 0) {
             for (int i = stars; i < outOf; i++) {
-                sb.append( ColourUtils.yellow(WHITE_STAR) + " " );
+                sb.append(ColourUtils.yellow(WHITE_STAR) + " ");
             }
         }
 
         return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printStars(0, 10));
+        System.out.println(printStars(2, 10));
+        System.out.println(printStars(4, 10));
+        System.out.println(printStars(9, 10));
     }
 
 }
