e2acbfe4e22344a71400a52df173a1d901a22c16
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-06-24 09:38:07 +0100

    Remove AnimalUtils.java - moved to branch-3a-static and simplify ZooPlayground

--- src/main/java/com/nology/ZooPlayground.java
+++ src/main/java/com/nology/ZooPlayground.java
@@ -11,38 +11,20 @@ public class ZooPlayground {
 
     public static void main(String[] args) {
 
-        Tiger tiger = new Tiger(AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge());
+        int nextId = 0;
+
+        Tiger tiger = new Tiger( nextId++, "Tony " + nextId, RANDOM.nextInt(10) + 2);
         System.out.println("Tiger " + tiger.getName() + " is " + tiger.getAge() + " years old");
         System.out.println(tiger);
 
         Zoo zoo = new Zoo();
-        // three tigers
-        for (int i = 0; i < 3; i++) {
-            zoo.addAnimal(new Tiger( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-        }
-        // five llamas
-        for (int i = 0; i < 5; i++) {
-            zoo.addAnimal(new Llama( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-        }
-        // 2 crocodiles
-        for (int i = 0; i < 5; i++) {
-            zoo.addAnimal(new Crocodile( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-        }
 
-        // or random!
-        for (int i = 0; i < 10; i++) {
-            switch (RANDOM.nextInt(3)) {
-                case 0:
-                    zoo.addAnimal(new Tiger( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-                    break;
-                case 1:
-                    zoo.addAnimal(new Llama( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-                    break;
-                case 2:
-                    zoo.addAnimal(new Crocodile( AnimalUtils.nextUniqueId(), AnimalUtils.generateName(), AnimalUtils.generateAge() ));
-                    break;
-            }
-        }
+        zoo.addAnimal( new Tiger( nextId++,"Terry", RANDOM.nextInt(10) + 2 ) );
+        zoo.addAnimal( new Tiger( nextId++,"Cuddles", RANDOM.nextInt(10) + 2 ) );
+        zoo.addAnimal( new Llama( nextId++, "Larry", RANDOM.nextInt(10) + 2 ));
+        zoo.addAnimal( new Llama( nextId++, "Legs", RANDOM.nextInt(10) + 2 ));
+        zoo.addAnimal( new Crocodile( nextId++, "Carol", RANDOM.nextInt(10) + 2 ));
+        zoo.addAnimal( new Crocodile( nextId++, "Snappy", RANDOM.nextInt(10) + 2 ));
 
         System.out.println( zoo.getAnimalCount() );
     }
