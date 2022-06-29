b5230d2896c15f0c401db02d95cc20e549b1dd79
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-06-29 17:52:51 +0100

    Copied in extra tests from abstract branch

--- src/test/java/com/nology/zoology/animal/AnimalTest.java
+++ src/test/java/com/nology/zoology/animal/AnimalTest.java
@@ -1,52 +1,97 @@
 package com.nology.zoology.animal;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
 
-    private Animal animal = new Tiger(42, "Tony", 7);
+    private static final int ANIMAL_ID = 1;
+    private static final String ANIMAL_NAME = "George";
+    private static final int ANIMAL_AGE = 7;
+
+    private Animal target;
+
+    public AnimalTest() {
+        System.out.println("Creating new AnimalTest!");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Creating new Animal");
+        this.target = buildAnonymousAnimal( ANIMAL_ID, ANIMAL_NAME, ANIMAL_AGE );
+    }
+
+    @AfterEach
+    void tidyUp() {
+        System.out.println("Sweeping up after animal");
+    }
+
+    @BeforeAll
+    static void beforeAllTests() {
+        System.out.println("Should run once");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Sweeping up after all animals!");
+    }
+
 
     @Test
+    @DisplayName("Get id is successful")
     void getId_ValidData_ReturnsId() {
-        assertEquals( 42, animal.getId() );
+        assertEquals( ANIMAL_ID, target.getId() );
     }
 
     @Test
+    @DisplayName("Set id is successful")
     void setId_ValidInput_Success() {
-        animal.setId(999);
-        assertEquals( 999, animal.getId() );
+        assertEquals( 1, target.getId() );
+        target.setId(2);
+        assertEquals( 2, target.getId() );
     }
 
     @Test
-    void getName_ValidData_ReturnsName() {
-        assertEquals( "Tony", animal.getName() );
+    @DisplayName("Tiger is hungry")
+    void isHungry_Hunger50_ReturnsTrue() {
+        target.setHunger((short) 50);
+
+        assertTrue( target.isHungry(), "Tiger is hungry" );
     }
 
     @Test
-    void setName_ValidInput_Success() {
-        animal.setName("Terry");
-        assertEquals( "Terry", animal.getName() );
+    @DisplayName("Tiger is not hungry")
+    void isHungry_Hunger10_ReturnsFalse() {
+        target.setHunger((short) 10);
+
+        assertFalse( target.isHungry(), "Tiger is not hungry" );
     }
 
     @Test
-    void getAge_ValidData_ReturnsId() {
-        assertEquals( 7, animal.getAge() );
+    @DisplayName("Feeding reduces hunger")
+    void feed_NoInputs_ReducesHunger() {
+        target.feed();
+        assertEquals(40, target.getHunger());
     }
 
     @Test
-    void setAge_ValidInput_Success() {
-        animal.setAge(12);
-        assertEquals( 12, animal.getAge() );
+    void testToString() {
+        assertEquals("[id=1, name='George', age=7]", target.toString());
     }
 
-    @Test
-    void makeSound() {
-    }
+    private Animal buildAnonymousAnimal(int id, String name, int age) {
+        return new Animal(id, name, age) {
+            @Override
+            public AnimalType getType() {
+                return AnimalType.lion;
+            }
 
-    @Test
-    void toString_ValidInput_ReturnsString() {
-        assertEquals("Tiger[id=42, name='Tony', age=7]", animal.toString());
+            @Override
+            public void makeSound() {
+                System.out.println("Roarrrrrr!!");
+            }
+        };
     }
 }
