ed17382b4d642e3eea5b10b8164e4b47dbea3967
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-07-07 12:12:19 +0100

    Added a try ... catch into loader, validation into Animal

--- src/main/java/com/nology/zoology/animal/Animal.java
+++ src/main/java/com/nology/zoology/animal/Animal.java
@@ -19,6 +19,8 @@ public abstract class Animal implements Comparable<Animal>, Starrable {
     protected short hunger = 50;
 
     public Animal(int id, String name, int age) {
+        validateAnimalName(name);
+        validateAnimalAge(age);
         this.id = id;
         this.name = name;
         this.age = age;
@@ -44,6 +46,15 @@ public abstract class Animal implements Comparable<Animal>, Starrable {
     }
 
     public void setName(String name) {
+        validateAnimalName(name);
+        this.name = name;
+    }
+
+    /**
+     * Validate the name for the animal, must be a valid String, not empty.
+     * @param name
+     */
+    private void validateAnimalName(String name) {
         if( name == null || "".equals(name.trim()) ) {
             throw new IllegalArgumentException("Name must be a valid name");
         }
@@ -55,7 +66,16 @@ public abstract class Animal implements Comparable<Animal>, Starrable {
     }
 
     public void setAge(int age) {
-        if( age < 1 || age > 99 ) {
+        validateAnimalAge(age);
+        this.age = age;
+    }
+
+    /**
+     * Validate the animal's age, must be between 1 and 99.
+     * @param age
+     */
+    private void validateAnimalAge(int age) {
+        if (age < 1 || age > 99) {
             throw new IllegalArgumentException("Age must be betweem 1 and 99 years");
         }
         this.age = age;
