bd96ba852336299ac3a00fcec97e862092eba17e
Stu Slicer <stu_slicer@yahoo.co.uk>
2022-07-07 09:30:33 +0100

    Move ZooPlayground to animal package

---
+++ src/main/java/com/nology/zoology/animal/ZooPlayground.java
@@ -0,0 +1,9 @@
+package com.nology.zoology.animal;
+
+public class ZooPlayground {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/com/nology/zoology/user/ZooPlayground.java
+++
@@ -1,5 +0,0 @@
-package com.nology.zoology.user;
-
-public class ZooPlayground {
-
-}
